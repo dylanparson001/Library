56f793baf8edec6d9c7d2f886eadbeadd8257317
Dylan Parson <dylan.parson001@gmail.com>
2021-03-28 07:32:33 -0400

    added styling

--- script.js
+++ script.js
@@ -15,14 +15,15 @@ function Book(title, author, pages, read) {
 
 function addToLibrary() {
   const newBook = new Book();
-  const bookForm = document.getElementById("newBook");
+  const bookForm = document.getElementById("newBookForm");
 
   newBook.title = bookForm[0].value;
   newBook.author = bookForm[1].value;
   newBook.pages = bookForm[2].value;
+  console.table(currentLibrary);
 
   currentLibrary.push(newBook);
-  document.getElementById("newBook").reset();
+  document.getElementById("newBookForm").reset();
   addCard();
 }
 
@@ -43,21 +44,26 @@ function addCard() {
     bookTitle.textContent = currentLibrary[i].title;
     bookAuthor.textContent = currentLibrary[i].author;
     bookPages.textContent = currentLibrary[i].pages;
-    bookRead.textContent = (currentLibrary[i].read ? "Finished" : "Not Finished");
-    bookRead.classList.add("readButtons");
+    bookRead.textContent = currentLibrary[i].read ? "Finished" : "Not Finished";
+    bookRead.classList.add("finished");
     deleteBook.setAttribute("value", i);
     deleteBook.textContent = "Delete Book";
+    deleteBook.classList.add("delete");
   }
 
   deleteBook.addEventListener("click", () => {
     deleteCurrentBook(deleteBook.value, booksElem);
   });
-  
-  bookRead.addEventListener("click", () =>{
-    currentLibrary[deleteBook.value].read = !currentLibrary[deleteBook.value].read;
-    bookRead.textContent = (currentLibrary[deleteBook.value].read ? "Finished" : "Not Finished");
+
+  bookRead.addEventListener("click", () => {
+    currentLibrary[deleteBook.value].read = !currentLibrary[deleteBook.value]
+      .read;
+    bookRead.textContent = currentLibrary[deleteBook.value].read
+      ? "Finished"
+      : "Not Finished";
     bookRead.classList.toggle("notFinished");
-  })
+    console.log(currentLibrary[deleteBook.value].read);
+  });
 
   newBook.append(bookTitle);
   newBook.append(bookAuthor);
@@ -65,61 +71,63 @@ function addCard() {
   newBook.append(bookRead);
   newBook.append(deleteBook);
   booksElem.append(newBook);
-
 }
 
 function deleteCurrentBook(value, booksElem) {
   // delete both the book in the currentLibrary array, along with on the DOM and refresh to show current library
   currentLibrary.splice(value, 1);
   booksElem.textContent = "";
-  
-  if (currentLibrary.length === 0) { // won't display empty table
+
+  if (currentLibrary.length === 0) {
+    // won't display empty table
     return;
   }
+
   displayNewLibrary();
 }
 
 /* same as addCard function, only the entire thing is in the loop.
    addCard will only display last index of currentLibrary */
 function displayNewLibrary() {
+  //DOM variables to create new Book card
   for (let i = 0; i < currentLibrary.length; i++) {
+    const booksElem = document.getElementById("books"); //selects books wrapper
+    const newBook = document.createElement("table"); // creates table
+    newBook.classList.add("bookCard");
 
-    //DOM variables to create new Book card
-  const booksElem = document.getElementById("books"); //selects books wrapper
-  const newBook = document.createElement("table"); // creates table
-  newBook.classList.add("bookCard");
+    const bookTitle = document.createElement("tr"); // creates table row for the title
+    const bookAuthor = document.createElement("tr"); // creates table row for the author
+    const bookPages = document.createElement("tr"); // creates table row for the pages
+    const bookRead = document.createElement("button"); // creates table row for if the user has read the book
+    const deleteBook = document.createElement("button");
 
-  const bookTitle = document.createElement("tr"); // creates table row for the title
-  const bookAuthor = document.createElement("tr"); // creates table row for the author
-  const bookPages = document.createElement("tr"); // creates table row for the pages
-  const bookRead = document.createElement("button"); // creates table row for if the user has read the book
-  const deleteBook = document.createElement("button");
-
-  for (let i = 0; i < currentLibrary.length; i++) {
     bookTitle.textContent = currentLibrary[i].title;
     bookAuthor.textContent = currentLibrary[i].author;
     bookPages.textContent = currentLibrary[i].pages;
-    bookRead.textContent = (currentLibrary[i].read ? "Finished" : "Not Finished");
-    bookRead.classList.add("readButtons");
+    bookRead.textContent = currentLibrary[i].read ? "Finished" : "Not Finished";
+    bookRead.classList.add("finished");
     deleteBook.setAttribute("value", i);
     deleteBook.textContent = "Delete Book";
+    deleteBook.classList.add("delete");
+
+    deleteBook.addEventListener("click", () => {
+      deleteCurrentBook(deleteBook.value, booksElem);
+    });
+
+    bookRead.addEventListener("click", () => {
+      currentLibrary[deleteBook.value].read = !currentLibrary[deleteBook.value]
+        .read;
+      bookRead.textContent = currentLibrary[deleteBook.value].read
+        ? "Finished"
+        : "Not Finished";
+      bookRead.classList.toggle("notFinished");
+    });
+
+    newBook.append(bookTitle);
+    newBook.append(bookAuthor);
+    newBook.append(bookPages);
+    newBook.append(bookRead);
+    newBook.append(deleteBook);
+    booksElem.append(newBook);
   }
-
-  deleteBook.addEventListener("click", () => {
-    deleteCurrentBook(deleteBook.value, booksElem);
-  });
-  
-  bookRead.addEventListener("click", () =>{
-    currentLibrary[deleteBook.value].read = !currentLibrary[deleteBook.value].read;
-    bookRead.textContent = (currentLibrary[deleteBook.value].read ? "Finished" : "Not Finished");
-    bookRead.classList.toggle("notFinished");
-  })
-
-  newBook.append(bookTitle);
-  newBook.append(bookAuthor);
-  newBook.append(bookPages);
-  newBook.append(bookRead);
-  newBook.append(deleteBook);
-  booksElem.append(newBook);
-}
 }
